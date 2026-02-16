c9d12f1a617b5d3c4a325c97e035c5b24afbe387
Sofia Zdobnikova <szdobnikova@fxcess.com>
2026-02-16 20:13:44 +0200

    add path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "node:path";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
+});

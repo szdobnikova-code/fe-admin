a0d4847a18ae19fcd58726e0eea286ef73d0df67
Sofia Zdobnikova <szdobnikova@fxcess.com>
2026-02-16 20:33:23 +0200

    add routing and base pages

---
+++ src/app/router/index.tsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from "react-router-dom";
+import { LoginPage } from "@/pages/login";
+import { ProductsPage } from "@/pages/products";
+
+export const router = createBrowserRouter([
+    { path: "/login", element: <LoginPage /> },
+    { path: "/products", element: <ProductsPage /> },
+    { path: "*", element: <LoginPage /> },
+]);
